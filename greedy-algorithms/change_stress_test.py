2876c6513ab600e6c9b4bdc9b71b399e20c36f0a
hristo-vrigazov <hvrigazov@gmail.com>
2016-06-04 19:46:46 +0300

    Fix test case generator error

--- greedy-algorithms/change_stress_test.py
+++ greedy-algorithms/change_stress_test.py
@@ -1,20 +1,10 @@
 from change import get_change
 
-number_of_ones = 1
+number_of_ones = 0
 number_of_fives = 0
 number_of_tens = 0
 
 for i in range(1, 1000):
-	expected = number_of_ones + number_of_fives + number_of_tens
-	actual = get_change(i)
-
-	print("Input: " + str(i))
-
-	if expected == actual:
-		print("OK")
-	else:
-		print("Fail! Expected " + str(expected) + ", but got " + str(actual) + " instead")
-
 	# if we have for example 9, (5, 1, 1, 1, 1), for 10
 	# we get only 10. It also works in other cases
 	if i % 10 == 0:
@@ -30,3 +20,13 @@ for i in range(1, 1000):
 		number_of_ones -= 4
 		continue
 	number_of_ones += 1
+
+	expected = number_of_ones + number_of_fives + number_of_tens
+	actual = get_change(i)
+
+	print("Input: " + str(i))
+
+	if expected == actual:
+		print("OK")
+	else:
+		print("Fail! Expected " + str(expected) + ", but got " + str(actual) + " instead")
