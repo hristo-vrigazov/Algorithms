c996f731f8970dc36c440b597a90542be7143cb7
hristo-vrigazov <hvrigazov@gmail.com>
2016-06-26 16:52:34 +0300

    Experimental majority_element; tests are yet to come

--- divide-and-conquer/majority_element.py
+++ divide-and-conquer/majority_element.py
@@ -6,7 +6,25 @@ def get_majority_element(a, left, right):
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+
+    half = (right - left) // 2
+    majority_left = get_majority_element(a, left, left + half)
+    majority_right = get_majority_element(a, left + half, right)
+
+    majority_left_counter = 0
+    majority_right_counter = 0
+
+    for v in a[left:right]:
+        if v == majority_left:
+            majority_left_counter += 1
+        if v == majority_right:
+            majority_right_counter += 1
+
+    if majority_left_counter > half:
+        return majority_left
+    if majority_right_counter > half:
+        return majority_right
+
     return -1
 
 if __name__ == '__main__':
